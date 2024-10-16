b1590468bd35ee1415ba31bef9c0a6112ee2bacc
Antim Maurya <109455435+Antim21@users.noreply.github.com>
2024-10-16 23:53:31 +0530

    Update voice assistant.py

--- voice assistant.py
+++ voice assistant.py
@@ -11,7 +11,7 @@ import smtplib
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[1].id
 
 
 def speak(audio):
